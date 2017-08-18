3218277a4beb0afcdbe3fe1ba48af9a47e3b59e4
Kristiyan <k.koshnicharov@abv.bg>
2017-08-18 16:14:22 +0300

    Test UTF-8

---
+++ Vignettes/src/vignettes/CarVin.java
@@ -0,0 +1,14 @@
+package vignettes;
+
+public class CarVin extends Vignette {
+
+	public CarVin(Period period) {
+		super(period);
+		this.dayPrice = 5;
+		this.stickTime = 5;
+		this.color = "Car-Blue";
+	}
+
+	
+
+}
